2c29d96f9f55b25300fda61f6162c1afacc477a6
Whoistolu <digittolu25@gmail.com>
2025-05-20 18:22:58 +0100

    Fetch the details of a specific ticket

--- app/routes/tickets/ticket.js
+++ app/routes/tickets/ticket.js
@@ -1,3 +1,37 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import gql from 'graphql-tag';
 
-export default class TicketsTicketRoute extends Route {}
+export default class TicketsTicketRoute extends Route {
+  @service apollo;
+
+  async model(params) {
+    const query = gql`
+      query($id: ID!) {
+        myTicket(id: $id) {
+          id
+          subject
+          status
+          description
+          comments {
+            id
+            body
+            author {
+              id
+              name
+              role
+            }
+            createdAt
+          }
+          attachments {
+            id
+            url
+            filename
+          }
+        }
+      }
+    `;
+
+    return this.apollo.query({ query, variables: { id: params.ticket_id } }, 'myTicket');
+  }
+}
