7d25f05534ef3ff25c0e618abf1f741392bb15aa
Whoistolu <digittolu25@gmail.com>
2025-05-20 17:53:33 +0100

    Generate base routes for customer portal: login, signup, tickets, ticket detail

--- app/router.js
+++ app/router.js
@@ -6,4 +6,10 @@ export default class Router extends EmberRouter {
   rootURL = config.rootURL;
 }
 
-Router.map(function () {});
+Router.map(function () {
+  this.route('login');
+  this.route('signup');
+  this.route('tickets', function () {
+    this.route('ticket');
+  });
+});
