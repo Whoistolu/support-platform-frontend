3c86a7be9462cc1315b5de7bda18d79980f9c499
Whoistolu <digittolu25@gmail.com>
2025-05-20 19:25:57 +0100

    Install and configure ActiveStorage JS for direct uploads

--- app/components/ticket-create-form.js
+++ app/components/ticket-create-form.js
@@ -2,7 +2,7 @@ import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import gql from 'graphql-tag';
 import { inject as service } from '@ember/service';
-import { upload } from 'ember-active-storage';
+import { uploadFile } from '../utils/direct-upload';
 
 export default class TicketCreateFormComponent extends Component {
   @service apollo;
@@ -11,64 +11,45 @@ export default class TicketCreateFormComponent extends Component {
   message = '';
   file = null;
 
-  @action updateSubject(e) {
-    this.subject = e.target.value;
-  }
-
-  @action updateMessage(e) {
-    this.message = e.target.value;
-  }
-
-  @action uploadFile(e) {
-    this.file = e.target.files[0];
-  }
+  @action updateSubject(e) { this.subject = e.target.value; }
+  @action updateMessage(e) { this.message = e.target.value; }
+  @action uploadFile(e) { this.file = e.target.files[0]; }
 
   @action async submitTicket(e) {
     e.preventDefault();
 
-    let signedId = null;
-
+    let fileSignedId = null;
     if (this.file) {
       try {
-        const uploadResult = await upload(this.file, {
-          directUploadUrl: 'http://localhost:3000/rails/active_storage/direct_uploads'
-        });
-
-        signedId = uploadResult.signedId;
-      } catch (uploadError) {
-        console.error('Upload failed:', uploadError);
-        return alert('File upload failed.');
+        fileSignedId = await uploadFile(this.file);
+      } catch (error) {
+        alert('File upload failed');
+        return;
       }
     }
 
     const mutation = gql`
       mutation($input: CreateTicketInput!) {
         createTicket(input: $input) {
-          ticket {
+          supportTicket {
             id
-            subject
+            title
             status
           }
+          errors
         }
       }
     `;
 
-    try {
-      const response = await this.apollo.mutate({
-        mutation,
-        variables: {
-          input: {
-            subject: this.subject,
-            message: this.message,
-            file: signedId
-          }
+    const response = await this.apollo.mutate({
+      mutation,
+      variables: {
+        input: {
+          title: this.subject,
+          description: this.message,
+          attachments: fileSignedId ? [fileSignedId] : []
         }
-      });
-
-      console.log('Ticket created:', response);
-    } catch (err) {
-      console.error('Ticket creation failed:', err);
-      alert('Something went wrong.');
-    }
+      }
+    });
   }
 }
