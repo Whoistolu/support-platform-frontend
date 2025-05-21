4cd44cbe86520d6ca4d49c79cd84f46dc9b5e3df
Whoistolu <digittolu25@gmail.com>
2025-05-21 13:36:16 +0100

    Create agent-daily-summary component

---
+++ app/components/agent-daily-summary.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+import query from '../graphql/queries/agent-open-tickets';
+
+export default class AgentDailySummaryComponent extends Component {
+  @service apollo;
+  @tracked tickets = [];
+
+  constructor() {
+    super(...arguments);
+    this.loadTickets();
+  }
+
+  async loadTickets() {
+    const response = await this.apollo.query({ query });
+    this.tickets = response.agentOpenTickets;
+  }
+}
