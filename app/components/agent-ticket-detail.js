a7c7fe7dacc6c5d1c422360f6835d562c2099086
Whoistolu <digittolu25@gmail.com>
2025-05-21 12:11:25 +0100

    Add mutation to pdate support ticket status

--- app/components/agent-ticket-detail.js
+++ app/components/agent-ticket-detail.js
@@ -1,40 +1,41 @@
-import Route from '@ember/routing/route';
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 import gql from 'graphql-tag';
 
-export default class AgentTicketDetailRoute extends Route {
+export default class AgentTicketDetailComponent extends Component {
   @service apollo;
 
-  async model(params) {
-    const query = gql`
-      query ($id: ID!) {
-        supportTicket(id: $id) {
-          id
-          title
-          description
-          status
-          createdAt
-          user {
-            email
-          }
-          comments {
+  @action
+  async updateStatus(newStatus) {
+    const mutation = gql`
+      mutation($id: ID!, $status: String!) {
+        updateSupportTicket(id: $id, status: $status) {
+          supportTicket {
             id
-            message
-            user {
-              name
-              role
-            }
-            createdAt
+            status
           }
+          errors
         }
       }
     `;
 
-    const result = await this.apollo.query({
-      query,
-      variables: { id: params.id },
+    await this.apollo.mutate({
+      mutation,
+      variables: {
+        id: this.args.ticket.id,
+        status: newStatus
+      },
+      update: (cache, { data }) => {
+        cache.modify({
+          id: cache.identify({ __typename: 'SupportTicket', id: this.args.ticket.id }),
+          fields: {
+            status() {
+              return newStatus;
+            }
+          }
+        });
+      }
     });
-
-    return result.supportTicket;
   }
 }
