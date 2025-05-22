6aa619f038337823b1cb5cb6430f6ad94f468cce
Whoistolu <digittolu25@gmail.com>
2025-05-22 21:35:07 +0100

    Redirect to tickets page if user is already authenticated

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,3 +1,13 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
-export default class LoginRoute extends Route {}
+export default class LoginRoute extends Route {
+  @service auth;
+  @service router;
+
+  beforeModel() {
+    if (this.auth.isAuthenticated()) {
+      this.router.transitionTo('tickets');
+    }
+  }
+}
