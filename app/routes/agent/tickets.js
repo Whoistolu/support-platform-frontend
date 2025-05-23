ddcc115e6fe6c07a7bf5dcd9330e86fa0accc23a
Whoistolu <digittolu25@gmail.com>
2025-05-23 11:16:10 +0100

    Redirect authenticated users away from login/signup

--- app/routes/agent/tickets.js
+++ app/routes/agent/tickets.js
@@ -1,3 +1,15 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import redirectIfNotAuthenticated from '../../../utils/redirect-if-not-authenticated';
 
-export default class AgentTicketsRoute extends Route {}
+export default class AgentTicketsRoute extends Route {
+  @service auth;
+  @service router;
+
+  beforeModel() {
+    if (redirectIfNotAuthenticated(this.auth, this.router)) return;
+    if (this.auth.user?.role !== 'agent') {
+      this.router.transitionTo('tickets');
+    }
+  }
+}
