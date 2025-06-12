9005d82fa4884e14b5d1dca5aa777511d8486c68
Whoistolu <digittolu25@gmail.com>
2025-06-12 23:39:54 +0100

    Set up the Apollo provider in src/apollo/client.js

---
+++ src/apollo/client.js
@@ -0,0 +1,13 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql',
+  credentials: 'include',
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+export default client;
